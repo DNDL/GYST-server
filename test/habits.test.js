8ea9440b4c1890cb0209e53fbdeb6f5186a1e8d3
Dan Hogan <hogan.daniel@hotmail.com>
2019-09-12 16:23:31 -0700

    added more routes and testing for habits

--- test/habits.test.js
+++ test/habits.test.js
@@ -4,6 +4,10 @@ const request = require('supertest');
 const app = require('../lib/app');
 const connect = require('../lib/utils/connect');
 const mongoose = require('mongoose');
+const User = require('../lib/models/User');
+const Habit = require('../lib/models/Habit');
+
+// jest.mock('../lib/middleware/ensure-auth.js');
 
 describe('test habit routes', () => {
   beforeAll(() => {
@@ -18,28 +22,74 @@ describe('test habit routes', () => {
     return mongoose.connection.close();
   });
 
-  const TEST_HABIT = {
-    owner: '3456d7sd456sa78',
-    title: 'Noahs habit',
-    frequency: 'Daily',
-    goal: 2,
-    why: 'Cause'
-  };
-
-  it('can create a habit using /POST', () => {
+  it('can create a habit using /POST', async done => {
+    const user = await User.create({ email: 'test@test.com' });
     return request(app)
       .post('/api/v1/habits')
-      .send(TEST_HABIT)
+      .send({ owner: user._id, title: 'Noahs habit', frequency: 'Daily', goal: 2, why: 'Cause' })
       .then(res => {
         expect(res.body).toEqual({
           _id: expect.any(String),
-          owner: '3456d7sd456sa78',
+          owner: user._id.toString(),
           title: 'Noahs habit',
           frequency: 'Daily',
           goal: 2,
           why: 'Cause',
           __v: 0
         });
+        done();
+      });
+  });
+
+  it('can get the current signed in users habits', async done => {
+    const user = await User.create({ email: 'test@test.com' });
+    await Habit.create({ owner: user._id, title: 'Test habit1', frequency: 'Weekly', goal: 2, why: 'Cause' });
+    await Habit.create({ owner: user._id, title: 'Test habit2', frequency: 'Daily', goal: 1, why: 'Cause' });
+    return request(app)
+      .get(`/api/v1/habits/user/${user._id}`)
+      .then(res => {
+        expect(res.body).toHaveLength(2);
+        done();
+      });
+  });
+
+  it('can update a habit', async done => {
+    const user = await User.create({ email: 'test@test.com' });
+    const habit = await Habit.create({ owner: user._id, title: 'Test habit1', frequency: 'Weekly', goal: 2, why: 'Cause' });
+    return request(app)
+      .patch(`/api/v1/habits/${habit._id}`)
+      .send({ frequency: 'Daily', goal: 22, why: 'FOR REASONS, DANNY', no: 'mister hogan' })
+      .then(res => {
+        console.log('updated habit in test', res.body);
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          owner: user._id.toString(),
+          title: 'Test habit1',
+          frequency: 'Daily',
+          goal: 22,
+          why: 'FOR REASONS, DANNY',
+          __v: 0
+        });
+        done();
+      });
+  });
+
+  it('can DUSTROYYYY a habit', async done => {
+    const user = await User.create({ email: 'test@test.com' });
+    const habit = await Habit.create({ owner: user._id, title: 'Test habit1', frequency: 'Weekly', goal: 2, why: 'Cause' });
+    return request(app)
+      .delete(`/api/v1/habits/${habit._id}`)
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          owner: user._id.toString(),
+          title: 'Test habit1',
+          frequency: 'Weekly',
+          goal: 2,
+          why: 'Cause',
+          __v: 0
+        });
+        done();
       });
   });
 });
---
+++ test/users.test.js
@@ -0,0 +1,37 @@
+require('dotenv').config();
+
+const request = require('supertest');
+const app = require('../lib/app');
+const connect = require('../lib/utils/connect');
+const mongoose = require('mongoose');
+// const User = require('../lib/models/User');
+
+
+describe('test habit routes', () => {
+  beforeAll(() => {
+    connect();
+  });
+
+  beforeEach(() => {
+    return mongoose.connection.dropDatabase();
+  });
+
+  afterAll(() => {
+    return mongoose.connection.close();
+  });
+
+  it('can create a user using /POST', () => {
+    return request(app)
+      .post('/api/v1/users')
+      .send({ email: 'aaaaaa@gmail.com' })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          friends: [],
+          friendInvitations: [],
+          email: 'aaaaaa@gmail.com',
+          __v: 0
+        });
+      });
+  });
+});
