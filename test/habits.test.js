b33b8f531f26ccc9ce20f2175479033e17bdd4a5
Dan Hogan <hogan.daniel@hotmail.com>
2019-09-15 09:56:19 -0700

    Fix habits tests

--- test/habits.test.js
+++ test/habits.test.js
@@ -26,7 +26,7 @@ describe('test habit routes', () => {
     const user = await User.create({ email: 'test@test.com' });
     return request(app)
       .post('/api/v1/habits')
-      .send({ owner: user._id, title: 'Noahs habit', frequency: 'Daily', goal: 2, why: 'Cause' })
+      .send({ owner: user._id, title: 'Noahs habit', frequency: 'Daily', goal: 2, days: { m: true }, color: 'blue', why: 'Cause' })
       .then(res => {
         expect(res.body).toEqual({
           _id: expect.any(String),
@@ -34,6 +34,8 @@ describe('test habit routes', () => {
           title: 'Noahs habit',
           frequency: 'Daily',
           goal: 2,
+          days: { m: true },
+          color: 'blue',
           why: 'Cause'
         });
         done();
@@ -42,8 +44,8 @@ describe('test habit routes', () => {
 
   it('can get the current signed in users habits', async done => {
     const user = await User.create({ email: 'test@test.com' });
-    await Habit.create({ owner: user._id, title: 'Test habit1', frequency: 'Weekly', goal: 2, why: 'Cause' });
-    await Habit.create({ owner: user._id, title: 'Test habit2', frequency: 'Daily', goal: 1, why: 'Cause' });
+    await Habit.create({ owner: user._id, title: 'Test habit1', frequency: 'Weekly', goal: 2, days: { m: true }, color: 'blue', why: 'Cause' });
+    await Habit.create({ owner: user._id, title: 'Test habit2', frequency: 'Daily', goal: 1, days: { m: true }, color: 'blue', why: 'Cause' });
     return request(app)
       .get('/api/v1/habits/')
       .then(res => {
@@ -54,7 +56,7 @@ describe('test habit routes', () => {
 
   it('can update a habit', async done => {
     const user = await User.create({ email: 'test@test.com' });
-    const habit = await Habit.create({ owner: user._id, title: 'Test habit1', frequency: 'Weekly', goal: 2, why: 'Cause' });
+    const habit = await Habit.create({ owner: user._id, title: 'Test habit1', frequency: 'Weekly', goal: 2, days: { m: true }, color: 'blue', why: 'Cause' });
     return request(app)
       .patch(`/api/v1/habits/${habit._id}`)
       .send({ frequency: 'Daily', goal: 22, why: 'FOR REASONS, DANNY', no: 'mister hogan' })
@@ -65,6 +67,8 @@ describe('test habit routes', () => {
           title: 'Test habit1',
           frequency: 'Daily',
           goal: 22,
+          days: { m: true },
+          color: 'blue',
           why: 'FOR REASONS, DANNY'
         });
         done();
@@ -73,7 +77,7 @@ describe('test habit routes', () => {
 
   it('can DUSTROYYYY a habit', async done => {
     const user = await User.create({ email: 'test@test.com' });
-    const habit = await Habit.create({ owner: user._id, title: 'Test habit1', frequency: 'Weekly', goal: 2, why: 'Cause' });
+    const habit = await Habit.create({ owner: user._id, title: 'Test habit1', frequency: 'Weekly', goal: 2, days: { m: true }, color: 'blue', why: 'Cause' });
     return request(app)
       .delete(`/api/v1/habits/${habit._id}`)
       .then(res => {
@@ -83,6 +87,8 @@ describe('test habit routes', () => {
           title: 'Test habit1',
           frequency: 'Weekly',
           goal: 2,
+          days: { m: true },
+          color: 'blue',
           why: 'Cause'
         });
         done();
