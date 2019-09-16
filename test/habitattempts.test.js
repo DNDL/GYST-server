fdc0cbcd86efaaaef282e2baa2f3a16e735b3aab
Dan Hogan <hogan.daniel@hotmail.com>
2019-09-16 14:07:54 -0700

    test and implementation of patching attempt by ID

--- test/habitattempts.test.js
+++ test/habitattempts.test.js
@@ -27,13 +27,31 @@ describe('test habit routes', () => {
     const user = await User.create({ email: 'test@test.com' });
     const habit = await Habit.create({ owner: user._id, title: 'Test habit1', frequency: 'Weekly', goal: 2, days: { m: true }, color: 'blue', why: 'Cause' });
     return request(app)
-      .post('/api/v1/habits/attempt/')
+      .post('/api/v1/habits/attempt')
       .send({ habit: habit._id })
       .then(res => {
         expect(res.body).toEqual({
           _id: expect.any(String),
           habit: habit._id.toString(),
           progress: 0,
+          createdAt: expect.any(String),
+          updatedAt: expect.any(String)
+        });
+      });
+  });
+
+  it('can update a habitAttempt using /PATCH', async() => {
+    const user = await User.create({ email: 'test@test.com' });
+    const habit = await Habit.create({ owner: user._id, title: 'Test habit1', frequency: 'Weekly', goal: 2, days: { m: true }, color: 'blue', why: 'Cause' });
+    const attempt = await HabitAttempt.create({ habit: habit._id });
+    return request(app)
+      .patch(`/api/v1/habits/attempt/${attempt._id}`)
+      .send({ progress: 1 })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          habit: habit._id.toString(),
+          progress: 1,
           createdAt: expect.any(String),
           updatedAt: expect.any(String)
         });
