cb37168c7c4a54e94dcefe80b9da11f3bb256b4a
Dan Hogan <hogan.daniel@hotmail.com>
2019-09-16 10:01:51 -0700

    Create test for posting habit attempt

---
+++ test/habitattempts.test.js
@@ -0,0 +1,42 @@
+require('dotenv').config();
+
+const request = require('supertest');
+const app = require('../lib/app');
+const connect = require('../lib/utils/connect');
+const mongoose = require('mongoose');
+const HabitAttempt = require('../lib/models/HabitAttempt');
+const User = require('../lib/models/User');
+const Habit = require('../lib/models/Habit');
+
+jest.mock('../lib/middleware/ensure-auth.js');
+
+describe('test habit routes', () => {
+  beforeAll(() => {
+    connect();
+  });
+
+  beforeEach(() => {
+    return mongoose.connection.dropDatabase();
+  });
+
+  afterAll(() => {
+    return mongoose.connection.close();
+  });
+
+  it('can create a habitAttempt using /POST', async() => {
+    const user = await User.create({ email: 'test@test.com' });
+    const habit = await Habit.create({ owner: user._id, title: 'Test habit1', frequency: 'Weekly', goal: 2, days: { m: true }, color: 'blue', why: 'Cause' });
+    return request(app)
+      .post('/api/v1/habits/attempt/')
+      .send({ habit: habit._id })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          habit: habit._id.toString(),
+          progress: 0,
+          createdAt: expect.any(String),
+          updatedAt: expect.any(String)
+        });
+      });
+  });
+});
